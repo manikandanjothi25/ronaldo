84c91dcf08dd1cb172c28e81a7946983e83fad42
manikandanjothi25 <30368801+manikandanjothi25@users.noreply.github.com>
2017-08-29 16:02:58 +0530

    Update odd or even.c

--- odd or even.c
+++ odd or even.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main()
+void main()
 {
     int number;
 
@@ -9,6 +9,4 @@ int main()
         printf("%d is even.", number);
     else
         printf("%d is odd.", number);
-
-    return 0;
 }
